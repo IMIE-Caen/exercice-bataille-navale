e31843ee843bf3a687ea4661cdce5842bbc97a08
Clément Alexandre <clmntlxndr@gmail.com>
2016-12-23 14:33:11 +0100

    Improvements

--- grille.php
+++ grille.php
@@ -2,23 +2,24 @@
 
 function dessiner_grille($tab){
   echo "\n\n";
-  dessiner_separateur($tab);
-  for($i = 0 ; $i < count($tab) ; $i = $i+1 ){
-    echo "| $tab[$i] ";
-    // Se je suis à la dernière case, je ferme le
-    // tableau
-    if($i == count($tab) - 1){
-      echo "|\n";
+  for($l = 0 ; $l < count($tab) ; $l++){
+    $ligne = $tab[$l];
+    dessiner_separateur(count($ligne));
+    for($i = 0 ; $i < count($ligne) ; $i++ ){
+      echo "| $ligne[$i] ";
+      // Se je suis à la dernière case, je ferme le
+      // tableau
+      if($i == count($ligne) - 1){
+        echo "|\n";
+      }
     }
+
   }
-  dessiner_separateur($tab);
+  dessiner_separateur(count($ligne));
   echo "\n\n";
 }
-function dessiner_separateur($tab){
-  for($i = 0 ; $i < count($tab) ; $i = $i+1 ){
-    echo "+---";
-    if($i == count($tab) - 1){
-      echo "+\n";
-    }
-  }
+
+
+function dessiner_separateur($nb_cases){
+  echo join('---', array_fill(0, $nb_cases + 1, '+'))."\n";
 }
--- main.php
+++ main.php
@@ -1,7 +1,10 @@
 <?php
-
 require_once("grille.php");
 
-$grille = array("a", "b", "c");
-
+$grille = array(
+      array("a", "b", "c"),
+      array("d", "e", "f"),
+      array("g", "h", "i"),
+      array("j", "k", "l")
+    );
 dessiner_grille($grille);
